c0aaba973a7e07671c006cd6d3e9355b48ad1457
ElPolloLoco <jeremie@nightlycoders.net>
2015-07-06 10:16:16 +0200

    feat

--- functions.php
+++ functions.php
@@ -1,15 +1,24 @@
 <?php
 require_once 'vendor/autoload.php';
+
 global $twig;
+
+//init admin
+/** @var \Controller\DancersController $dancers_controller */
+$dancers_controller = Services\SingletonFactory::getInstance('\Controller\DancersController');
+$dancers_controller->init();
+/** @var \Controller\Battle $dancers_controller */
+$dancers_controller = Services\SingletonFactory::getInstance('\Controller\Battle');
+$dancers_controller->init();
 
 // init theme
 
 
 // theme constants
 
-
+// Load global Twig instance
 if(!$twig){
-    $loader = new Twig_Loader_Filesystem(__DIR__.'/ressources/views'); // Dossier contenant les templates
+    $loader = new Twig_Loader_Filesystem(__DIR__.'/ressources/views');
     $twig = new Twig_Environment($loader, array(
         'cache' => false
     ));
